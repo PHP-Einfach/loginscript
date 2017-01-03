9396d59ea77d34fc80233889f81813736d9d9b1d
Nils Reimers <rnils@web.de>
2017-01-03 14:27:29 +0100

    Update and rename users.sql to database.sql

--- database.sql
+++ database.sql
@@ -16,6 +16,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-
+CREATE TABLE `securitytokens` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` int(10) NOT NULL,
+  `identifier` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `securitytoken` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
